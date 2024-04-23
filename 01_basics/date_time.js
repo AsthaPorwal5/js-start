0ecd96f7b1bc274f5cc8fe628d089c6e7cf5808c
AsthaPorwal5 <98209497+AsthaPorwal5@users.noreply.github.com>
2024-04-23 09:07:29 +0000

    config conatiner done

--- 01_basics/date_time.js
+++ 01_basics/date_time.js
@@ -1,5 +1,22 @@
 let mydate = new Date()
-console.log(mydate.toString());
-console.log(mydate.toISOString());
-console.log(mydate.toLocaleDateString());
-console.log(mydate.toLocaleString());
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+let mycreateddate =new Date(2024,1,26);
+//console.log(mycreateddate.toDateString());
+let createdate = new Date(2025,0,23);
+//console.log(createdate.toDateString());
+//************timestamp *************
+
+let mytimestamp = Date.now();
+// console.log(mytimestamp);
+//  console.log(mycreateddate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+console.log(newdate.getDay());
