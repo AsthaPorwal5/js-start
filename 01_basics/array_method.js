c120f9233815b995fa82d139c4f0c26e9fd01a6f
AsthaPorwal5 <98209497+AsthaPorwal5@users.noreply.github.com>
2024-04-26 05:35:42 +0000

    container confihure done

---
+++ 01_basics/array_method.js
@@ -0,0 +1,14 @@
+let names=["Rahul","Atul","Sanjay","Sumit"];
+//console.log(names);
+names.push("Gyanu","Vineet");
+//console.log(names);
+names.pop();
+//console.log(names);
+names.unshift("MCA Faculty --");
+//console.log(names);
+names.shift();
+//console.log(names);
+console.log(names);
+let some_names=names.slice(1,3);
+console.log(some_names);
+
