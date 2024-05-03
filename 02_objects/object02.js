fad66444c5b452e67348b0c37f6465f6b9495909
AsthaPorwal5 <98209497+AsthaPorwal5@users.noreply.github.com>
2024-05-03 09:37:38 +0000

    configure complete done

---
+++ 02_objects/object02.js
@@ -0,0 +1,18 @@
+//const tinderUser = new Object()
+
+// const tinderUser = {} 
+// tinderUser.id = "1234abcd"
+// tinderUser.name = "John"
+// tinderUser.isloggedin = false
+// console.log(tinderUser);
+const regularUser ={
+    email : "john123@gmail.com",
+    fullname: {
+        userfullname : {
+            firstname : "john",
+            lastname : " singh"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname);
