dc77282b2a1624bed0e9038636664b8f8ab7a01d
AsthaPorwal5 <98209497+AsthaPorwal5@users.noreply.github.com>
2024-06-03 06:14:16 +0000

    configure done

---
+++ 02_objects/function02.js
@@ -0,0 +1,28 @@
+function calculatecartprice(val1,val2,...num1){
+   //console.log(val1);
+    return num1;
+}
+//console.log(calculatecartprice(200,400,500))
+
+// const user={
+//     username:"sam ",
+//     price:199
+// }
+
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+// }
+//handleobject(user)
+// handleobject({
+//     username: "sam",
+//     price: 399
+// })
+
+const mynewarray= [ 200,400,600]
+function returnsecondvalue(getarray){
+    return getarray[0]
+
+}
+//console.log(returnsecondvalue(mynewarray));
+console.log(returnsecondvalue([200,400,600,800]));
