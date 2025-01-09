b098af6ba3d7b3a2cef387641d9d72354d7a92d9
AsthaPorwal5 <98209497+AsthaPorwal5@users.noreply.github.com>
2025-01-09 16:36:39 +0530

    Update 01_variable.js

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,7 +3,7 @@ let accountEmail = "astha12345@gmail.com"
 var accountPassword ="12345"
 accountCity = "Jaipur"
 
-// accountId = 2
+// accountId = 2 nkreff
 accountPassword = 45623
 console.log(accountPassword);
 console.table([accountEmail,accountId,accountPassword,accountCity])
